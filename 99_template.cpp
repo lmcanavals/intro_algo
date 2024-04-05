7dd9504bc4cee44360f06c0fd2b61abcde0df547
Luis M Canaval S <public02@canaval.org>
2024-04-04 21:45:08 -0500

    added template

---
+++ 99_template.cpp
@@ -0,0 +1,20 @@
+/*
+ * 
+ * */
+#include <iostream>
+
+using  namespace std;
+
+// declaraciones de prototipos
+
+
+
+// Punto de entrada del programa
+int main() {
+
+	return EXIT_SUCCESS;
+}
+
+// Implementación de funciones
+
+
